eafd9f86cbe3a1a9833f84708eb8c044d1dd110f
Alessandro Limardo <alessandro.limardo@kaleyra.com>
2023-08-04 10:43:55 +0200

    Temporary remove native-bridge

--- ios/native-bridge/TypeScript/errors/IllegalArgumentError.ts
+++
@@ -1,9 +0,0 @@
-// Copyright © 2018-2023 Kaleyra S.p.a. All Rights Reserved.
-// See LICENSE for licensing information
-
-/**
- * This error will be thrown when arguments given to a function are not of the expected type
- */
-export class IllegalArgumentError extends Error {
-  name = 'IllegalArgumentError';
-}
--- ios/native-bridge/android/src/main/java/com/kaleyra/video_hybrid_native_bridge/events/EventsReporter.kt
+++
@@ -1,9 +0,0 @@
-// Copyright © 2018-2023 Kaleyra S.p.a. All Rights Reserved.
-// See LICENSE for licensing information
-
-package com.kaleyra.video_hybrid_native_bridge.events
-
-interface EventsReporter {
-    fun start()
-    fun stop()
-}
